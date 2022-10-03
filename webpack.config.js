09068b7acf3c8a00efeac6f17ef7648f99f7759a
Huib van Geertruy <huib.vangeertruy@rr-wfm.com>
2022-10-03 09:41:49 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "rrwfm",
+    projectName: "rr-notification",
+    webpackConfigEnv,
+    argv,
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    devServer: {
+      port: 8012,
+      server: "https",
+    },
+  });
+
+};
